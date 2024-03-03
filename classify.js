cfde51cda9bed23533b5208db18aa2040933db7d
TheODDYSEY <mosessabz07@gmail.com>
2024-03-03 22:28:40 +0300

    img read func 🥸

--- classify.js
+++ classify.js
@@ -2,3 +2,5 @@ const tf = require('@tensorflow/tfjs');
 const mobilenet = require('@tensorflow-models/mobilenet');
 const tfnode = require('@tensorflow/tfjs-node');
 const fs = require('fs');
+
+// raed the image 
